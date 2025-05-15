248a9e8d47da744956f1d8e1c337298581a95bd9
YumpA <russki-misha@mail.ru>
2025-05-15 14:47:14 +0500

    create AuthorizationForm

---
+++ src/components/AuthorizationForm.vue
@@ -0,0 +1,32 @@
+<script>
+    export default {
+        methods:{
+            Auth(){
+                
+                if (this.login == '' || this.pass == ''){
+                    this.error ="Введите поле"
+                    return;
+                }
+                this.error=''
+                this.users.push({
+                    name:this.name,
+                    pass:this.pass,
+                    mail:this.mail
+                })
+            }
+    }
+    }
+    
+</script>
+
+    <template>
+        <form>
+            <label>Вход</label>
+            <input type ="login" v-model="login" placeholder="Логин"/>
+            <input type ="password" v-model="pass" placeholder="Пароль"/>
+            <button @click="Auth()">Войти</button>
+        </form>
+    </template>
+
+<style>
+</style>
